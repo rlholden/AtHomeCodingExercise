681cb3e9eb67aa0150567adeb49278b030535fb0
rlh066 <rholden@amfam.com>
2023-01-22 12:08:33 -0600

    Add XML for AboveBelowResult object

--- AtHomeCodingExerciseBaseCode/Objects/AboveBelowResult.cs
+++ AtHomeCodingExerciseBaseCode/Objects/AboveBelowResult.cs
@@ -1,9 +1,24 @@
 ﻿namespace AtHomeCodingExerciseBaseCode.Objects
 {
+    /// <summary>
+    /// Object to return for exercise.
+    /// </summary>
     public class AboveBelowResult
     {
+        /// <summary>
+        /// The count of integers whose values are above the comparison value.
+        /// </summary>
         public int Above { get; set; } = 0;
+
+        /// <summary>
+        /// The number of integers whose values are below the comparison value.
+        /// </summary>
         public int Below { get; set; } = 0;
+
+        /// <summary>
+        /// Override of string rendering for display purposes.
+        /// </summary>
+        /// <returns>Readable format of the integer counts.</returns>
         public override string ToString()
         {
             return $"{{ \"above\": {Above}, \"below\": {Below} }}";
